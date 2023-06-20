55146695ceeb05d7cbb38190044edac11df637b8
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-06-20 16:40:10 +0530

    prom demo

---
+++ promusesdemo.js
@@ -0,0 +1,13 @@
+const nums = [565, 25, 85, 55, 33]
+const mProm = new Promise((resolve, reject) => {
+    let no = nums.find((n) => n == 33)
+    if (no != null) {
+        setTimeout(() => { resolve(no) }, 5000)
+    } else {
+        reject(new Error("Not FOund"))
+    }
+})
+mProm
+    .then((data) => console.log("Data", data))
+    .catch((err) => console.log(err))
+    .finally(() => console.log("Settled"))
